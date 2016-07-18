1ea41b98aa61d52ae6d41ef2c72912288909fa1f
kotemaru <kotemaru@kotemaru.org>
2016-07-18 06:53:46 +0000

    prepare

--- test/test1.js
+++ test/test1.js
@@ -4,19 +4,21 @@ var assert = require('assert');
 var GeneratorHelper = require('../GeneratorHelper');
 var Fs = require('fs');
 
-describe('GeneratorHelper', function () {
+describe('GeneratorHelper', function() {
     this.timeout(5000);
-    describe('Normal case', function () {
-        it('a', function (done) {
+    describe('Normal case', function() {
+        it('a', function(done) {
             let count = 0;
-            function callback(err,val) {
-                console.log('---->',err,val);
-                assert.equal(err,null,"err");
-                assert.equal(val,"abcdefg123456789");
+
+            function callback(err, val) {
+                console.log('---->', err, val);
+                assert.equal(err, null, "err");
+                assert.equal(val, "abcdefg123456789");
                 count++;
             }
-            function callbackDone(err,val) {
-                callback(err,val);
+
+            function callbackDone(err, val) {
+                callback(err, val);
                 assert.equal(count, 8, "count");
                 done();
             }
@@ -30,7 +32,36 @@ describe('GeneratorHelper', function () {
             GeneratorHelper.exec(generator1, callback)("test/test.txt", 1234);
         });
     });
-
+    describe('Fail case', function() {
+        it('a', function(done) {
+            function callback(err, val) {
+                console.log('---->', err, val);
+                try {
+                    assert.notEqual(err, null, "err");
+                    done();
+                }
+                catch (e) {
+                    done(e);
+                }
+            }
+            GeneratorHelper.exec(generator1, callback)("not-found.txt", 0);
+        });
+    });
+    describe('Fail case2', function() {
+        it('a', function(done) {
+            function callback(err, val) {
+                console.log('---->', err, val);
+                try {
+                    assert.notEqual(err, null, "err");
+                    done();
+                }
+                catch (e) {
+                    done(e);
+                }
+            }
+            GeneratorHelper.exec(generator1, callback)("test/test.txt", -1);
+        });
+    });
 });
 
 
@@ -46,8 +77,9 @@ function* generator1(fileName, time) {
 
 function* sleep(time) {
     if (time == 0) return 0;
+    if (time < 0) throw "bad time";
     let self = this;
-    yield setTimeout(function(){
+    yield setTimeout(function() {
         self.next();
     }, time);
     return time;
